6bbce3c105b90270eb1b5faf4192986967408238
The Breaker <thebreaker@Thes-MacBook-Air.local>
2021-05-13 23:00:31 +0700

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Image from './assets/test.png'
+
+function App() {
+    return (
+        <div>
+            <img src={Image}></img>
+            Hello World!!!
+        </div>
+    )
+}
+
+export default App;
